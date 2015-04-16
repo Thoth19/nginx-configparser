7ee2e932fd185bc07f415d15d9806cc1b1fa1ad3
Kyle <kpseipp@gmail.com>
2015-04-15 17:20:40 -0700

    Current work on set2

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,43 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
-
+//this is an edit
 TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
 
   bool success = parser.Parse("example_config", &out_config);
-
   EXPECT_TRUE(success);
+}
+TEST(NginxConfigParserTest, ToString) {
+  NginxConfigStatement statement;
+  statement.tokens_.push_back("foo");
+  statement.tokens_.push_back("bar");
+
+  EXPECT_EQ("foo bar;\n", statement.ToString(0));
+}
+class NginxStringConfigTest : public ::testing::Test {
+protected:
+    bool ParseString(const std::string& config_string) {
+        std::stringstream config_stream(config_string);
+        return parser_.Parse(&config_stream, &config_);
+    }
+    NginxConfigParser parser_;
+    NginxConfig config_;
+    
+};
+
+TEST_F(NginxStringConfigTest, SimpleTextConfig){
+    EXPECT_TRUE(ParseString("foo bar;"));
+    EXPECT_EQ(1, config_.statements_.size());
+}
+TEST_F(NginxStringConfigTest, SimpleBadTextConfig){
+    EXPECT_FALSE(ParseString("foo bar"));
+}
+TEST_F(NginxStringConfigTest, NestedConfig){
+    EXPECT_TRUE(ParseString(
+        "foo { "
+        "bar {"
+         "baz qux;"
+         "}"
+         "}"));
 }
